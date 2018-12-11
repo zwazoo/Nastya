57d43574eb22012f794de72249a15fc2c14ea13e
zwazoo <liudmilla.kharchenko@gmail.com>
2018-12-11 17:37:10 +0200

    added contact page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-    $('#nav-icon').click(function(){
-        $(this).toggleClass('open');
-    });
+
+    // $('.ba-form').validate({
+    //     rules:
+    //     {
+    //         mail:{
+    //             required: true,
+    //         },
+    //     }
+
+    // });
+
+
 });
